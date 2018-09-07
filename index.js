c0cdd16ccb081a12be528ec6d77430b3504ba6d5
EternityTula <eternity.tula@gmail.com>
2018-09-08 00:37:23 +0300

    copays mod

--- index.js
+++ index.js
@@ -6,46 +6,48 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib-btcz found. ' +
-      'Please make sure to require bitcore-lib-btcz and check that submodules do' +
-      ' not also include their own bitcore-lib-btcz dependency.';
+    var message = 'More than one instance of bitcore-lib-btcz-mini found. ' +
+      'Please make sure to require bitcore-lib-btcz-mini and check that submodules do' +
+      ' not also include their own bitcore-lib-btcz-mini dependency.';
     throw new Error(message);
   }
 };
 bitcore.versionGuard(global._bitcoreBtcz);
 global._bitcoreBtcz = bitcore.version;
 
-// crypto
+// crypto                          	
 bitcore.crypto = {};
-bitcore.crypto.BN = require('./lib/crypto/bn');
+// bitcore.crypto.BN = require('./lib/crypto/bn');
 bitcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bitcore.crypto.Hash = require('./lib/crypto/hash');
-bitcore.crypto.Random = require('./lib/crypto/random');
-bitcore.crypto.Point = require('./lib/crypto/point');
-bitcore.crypto.Signature = require('./lib/crypto/signature');
+bitcore.crypto.Hash = require('bitcore-lib').crypto.Hash;
+// bitcore.crypto.Random = require('bitcore-lib').crypto.Random;
+// bitcore.crypto.Point = require('bitcore-lib').crypto.Point ;
+bitcore.crypto.Signature = require('bitcore-lib').crypto.Signature;
 
 // encoding
-bitcore.encoding = {};
-bitcore.encoding.Base58 = require('./lib/encoding/base58');
-bitcore.encoding.Base58Check = require('./lib/encoding/base58check');
-bitcore.encoding.BufferReader = require('./lib/encoding/bufferreader');
-bitcore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-bitcore.encoding.Varint = require('./lib/encoding/varint');
+ bitcore.encoding = {};
+bitcore.encoding.Base58 = require('bitcore-lib').encoding.Base58;
+bitcore.encoding.Base58Check = require('bitcore-lib').encoding.Base58Check;
+bitcore.encoding.BufferReader = require('bitcore-lib').encoding.BufferReader;
+bitcore.encoding.BufferWriter = require('bitcore-lib').encoding.BufferWriter;
+// bitcore.encoding.Varint = require('bitcore-lib').encoding.Varint;
 
 // utilities
-bitcore.util = {};
-bitcore.util.buffer = require('./lib/util/buffer');
-bitcore.util.js = require('./lib/util/js');
-bitcore.util.preconditions = require('./lib/util/preconditions');
+// bitcore.util = {};
+// bitcore.util.buffer = require('bitcore-lib').buffer;
+// bitcore.util.js = require('bitcore-lib').util.js;
+// bitcore.util.preconditions = require('bitcore-lib').util.preconditions;
+// bitcore.util.base32 = require('bitcore-lib').util.base32;
+// bitcore.util.convertBits = require('bitcore-lib').util.convertBits;
 
 // errors thrown by the library
-bitcore.errors = require('./lib/errors');
+// bitcore.errors = require('./lib/errors');
 
 // main bitcoin library
 bitcore.Address = require('./lib/address');
-bitcore.Block = require('./lib/block');
-bitcore.MerkleBlock = require('./lib/block/merkleblock');
-bitcore.BlockHeader = require('./lib/block/blockheader');
+// bitcore.Block = require('./lib/block');
+// bitcore.MerkleBlock = require('./lib/block/merkleblock');
+// bitcore.BlockHeader = require('./lib/block/blockheader');
 bitcore.HDPrivateKey = require('./lib/hdprivatekey.js');
 bitcore.HDPublicKey = require('./lib/hdpublickey.js');
 bitcore.Networks = require('./lib/networks');
@@ -66,4 +68,5 @@ bitcore.deps.elliptic = require('elliptic');
 bitcore.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-bitcore.Transaction.sighash = require('./lib/transaction/sighash');
+//bitcore.Transaction.sighash = require('./lib/transaction/sighash');
+module.exports = bitcore;
